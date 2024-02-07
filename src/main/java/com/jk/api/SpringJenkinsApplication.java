b8d1a5e1ac36b7f339c65d9b5a0564771914bba1
KomalJugale <komal.jugale95@gmail.com>
2024-02-07 08:38:19 +0000

    first commit

---
+++ src/main/java/com/jk/api/SpringJenkinsApplication.java
@@ -0,0 +1,33 @@
+package com.jk.api;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.annotation.PostConstruct;
+
+@SpringBootApplication
+@RestController
+public class SpringJenkinsApplication {
+	
+	public static Logger logger = LoggerFactory.getLogger(SpringJenkinsApplication.class);
+	
+	@PostConstruct
+	public void init() {
+		logger.info("Application started.. ");
+	}
+	
+    @GetMapping("/app")
+	public String display() {
+		return "this is my first jenkin deployment";
+	}
+	
+	public static void main(String[] args) {
+		logger.info("Application executed ..");
+		SpringApplication.run(SpringJenkinsApplication.class, args);
+	}
+
+}
